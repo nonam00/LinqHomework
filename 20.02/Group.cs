bbbf16e65c1f07626c4a1c47db15c2061384e327
nonam00 <raiskt.ilya3@yandex.ru>
2024-02-11 21:20:44 +0300

    Добавьте файлы проекта.

---
+++ 20.02/Group.cs
@@ -0,0 +1,8 @@
+﻿namespace _20._02
+{
+	public class Group
+	{
+		public string Title { get; set; }
+		public List<Student> Students { get; set; } = new List<Student>();
+	}
+}
---
+++ 20.02/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace _20._02
+{
+	public class Student
+	{
+		public string Name { get; set; }	
+		public int Age { get; set; }
+	}
+}
